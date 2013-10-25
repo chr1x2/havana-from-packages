009f1964181ef92bcad9265cb7488bf93cb4200d
Christian Foronda <christian@foronda.org>
2013-10-25 18:37:45 +0800

    just listen to 127.0.0.1

--- install-mysql.sh
+++ install-mysql.sh
@@ -7,6 +7,6 @@ debconf-set-selections <<< "mysql-server mysql-server/root_password_again passwo
 
 apt-get install --yes python-mysqldb mysql-server
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/my.cnf
+#sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/my.cnf
 
 service mysql restart
